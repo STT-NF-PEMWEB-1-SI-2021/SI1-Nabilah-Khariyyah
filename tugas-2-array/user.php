2c537177d35db5552b3473cfd8a9318ed926a71f
nabilahkh <92252191+nabilahkh@users.noreply.github.com>
2022-02-25 13:16:13 +0700

    Add files via upload

---
+++ tugas-2-array/user.php
@@ -0,0 +1,13 @@
+<?php
+    // mendefinisikan variabel
+    $nama = 'Nabilah Khariyyah';
+    $umur = 19;
+    $berat = 48.5;
+
+    echo 'Nama : ' . $nama;
+    echo '<br/>Umur : ' . $umur. ' Tahun';
+    echo '<br/> Berat : ' . $berat. ' Kg';
+
+    echo "<br/><br/>Hello $nama Apa kabar";
+
+?>
